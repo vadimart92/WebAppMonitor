29ab9621aecaa711031ff630e5fb779a88772aed
Vadym <vadimart92@gmail.com>
2017-07-04 22:00:30 +0300

    clear

--- SQL/sp_ReplaceRecord.sql
+++ SQL/sp_ReplaceRecord.sql
@@ -1,11 +1,11 @@
 ﻿DROP PROCEDURE IF EXISTS sp_ReplaceRecord;
-GO;
+GO
 CREATE PROCEDURE sp_ReplaceRecord (@tableName NVARCHAR(MAX), @mapTableName NVARCHAR(MAX)) AS BEGIN
 
 	DECLARE @SQL NVARCHAR(MAX);
 	
 	DECLARE cur CURSOR FAST_FORWARD READ_ONLY LOCAL FOR
-	SELECT 'UPDATE ' + t.name + ' SET ' + c.name + ' = map.NewId FROM ['+@mapTableName+'] map WHERE ' + c.name + ' = map.OldId' AS query
+	SELECT 'UPDATE [' + t.name + '] SET [' + c.name + '] = map.NewId FROM ['+@mapTableName+'] map WHERE [' + c.name + '] = map.OldId' AS query
 	FROM sys.foreign_key_columns AS fk
 	INNER JOIN sys.tables AS t ON fk.parent_object_id = t.object_id
 	INNER JOIN sys.columns AS c ON fk.parent_object_id = c.object_id
@@ -35,70 +35,62 @@ CREATE PROCEDURE sp_ReplaceRecord (@tableName NVARCHAR(MAX), @mapTableName NVARC
 
 	COMMIT TRANSACTION
 END;
-
+GO
 --usage
-CREATE TABLE #map (OldId UNIQUEIDENTIFIER, NewId UNIQUEIDENTIFIER)ж
+---CREATE TABLE #map (OldId UNIQUEIDENTIFIER, NewId UNIQUEIDENTIFIER)ж
 --insert
-EXEC sp_ReplaceRecord @tableName='NormQueryTextHistory', @mapTableName='#map'
-DROP TABLE #map
+--EXEC sp_ReplaceRecord @tableName='NormQueryTextHistory', @mapTableName='#map'
+--DROP TABLE #map
 
 
 -- Trim text or replace by existing
+DROP PROCEDURE IF EXISTS sp_TrimNormalizedQueries;
+GO
+CREATE PROCEDURE sp_TrimNormalizedQueries AS BEGIN
 
-DROP TABLE IF EXISTS trimData;
-GO;
+	DROP TABLE IF EXISTS trimData;
 
-BEGIN TRANSACTION
+	BEGIN TRANSACTION
 
-SELECT nqth.NormalizedQuery OldQuery
-		,norm.NormalizedQuery NewQuery
-		, nqth.Id OldQueryId 
-		, norm.Id NewQueryId
-INTO trimData
-FROM NormQueryTextHistory nqth
-LEFT JOIN NormQueryTextHistory norm ON norm.QueryHash = HASHBYTES('SHA2_512', RTRIM(LTRIM(nqth.NormalizedQuery)))
-WHERE DATALENGTH(nqth.NormalizedQuery) <> DATALENGTH(RTRIM(LTRIM(nqth.NormalizedQuery)));
+	SELECT nqth.NormalizedQuery OldQuery
+			,norm.NormalizedQuery NewQuery
+			, nqth.Id OldQueryId 
+			, norm.Id NewQueryId
+	INTO trimData
+	FROM NormQueryTextHistory nqth
+	LEFT JOIN NormQueryTextHistory norm ON norm.QueryHash = HASHBYTES('SHA2_512', RTRIM(LTRIM(nqth.NormalizedQuery)))
+	WHERE DATALENGTH(nqth.NormalizedQuery) <> DATALENGTH(RTRIM(LTRIM(nqth.NormalizedQuery)));
 
-UPDATE NormQueryTextHistory
-SET NormalizedQuery = RTRIM(LTRIM(NormalizedQuery))
-	,QueryHash = HASHBYTES('SHA2_512', RTRIM(LTRIM(NormalizedQuery)))
-FROM trimData d
-WHERE d.NewQueryId is NULL
-	AND Id = d.OldQueryId;
-
-DELETE FROM NormQueryTextSource
-WHERE EXISTS (
-	SELECT h.QueryHistoryId
+	UPDATE NormQueryTextHistory
+	SET NormalizedQuery = RTRIM(LTRIM(NormalizedQuery))
+		,QueryHash = HASHBYTES('SHA2_512', RTRIM(LTRIM(NormalizedQuery)))
 	FROM trimData d
-	INNER JOIN QueryTextHistory h ON h.NormQueryTextHistoryId = d.NewQueryId
-	WHERE d.OldQueryId = NormQueryTextId
-)
+	WHERE d.NewQueryId is NULL
+		AND Id = d.OldQueryId;
 
+	DELETE FROM NormQueryTextSource
+	WHERE EXISTS (
+		SELECT h.QueryHistoryId
+		FROM trimData d
+		INNER JOIN QueryTextHistory h ON h.NormQueryTextHistoryId = d.NewQueryId
+		WHERE d.OldQueryId = NormQueryTextId
+	)
 
-DECLARE @oldId UNIQUEIDENTIFIER, @newId UNIQUEIDENTIFIER
+	CREATE TABLE #map (OldId UNIQUEIDENTIFIER, NewId UNIQUEIDENTIFIER);
 
-DECLARE cur CURSOR FAST_FORWARD READ_ONLY LOCAL FOR
-SELECT OldQueryId, NewQueryId
-FROM trimData
-WHERE NewQueryId IS NOT NULL
+	INSERT INTO #map
+	SELECT OldQueryId, NewQueryId
+	FROM trimData
+	WHERE NewQueryId IS NOT NULL
 
-OPEN cur
+	EXEC sp_ReplaceRecord @tableName='NormQueryTextHistory', @mapTableName='#map'
+	DROP TABLE #map
 
-FETCH NEXT FROM cur INTO @oldId, @newId
-
-WHILE @@FETCH_STATUS = 0 BEGIN
-
-	EXEC ReplaceRecord @tableName = N'NormQueryTextHistory'
-					   ,@oldValue = @oldId
-					   ,@newValue = @newId
-	FETCH NEXT FROM cur INTO @oldId, @newId
+	COMMIT TRANSACTION
 
 END
 
-CLOSE cur
-DEALLOCATE cur
-
-COMMIT TRANSACTION
+--exec sp_TrimNormalizedQueries
 
 SELECT COUNT(*)
 FROM NormQueryTextHistory nqth
